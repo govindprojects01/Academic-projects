952d06b0a4a2d71a65aeb499cc7fd8d20085cb5e
Karan Kumar Prajapati <kk2963155@gmail.com>
2026-09-11 13:33:26 +0530

    Fix middleware invocation error by restoring clean clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publishableKey:
-    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
-    "pk_test_cmVsaWV2ZWQtYmVkYnVnLTI4MzcuY2xlcmsuYWNjb3VudHMuZGV2JA",
-  secretKey:
-    process.env.CLERK_SECRET_KEY ||
-    "sk_test_XQlHFhjUhSYwSrpGwIVLR8uEwjUWpqqPLLeraYhIia",
-});
+export default clerkMiddleware();
 
 export const config = {
-  runtime: "nodejs",
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
